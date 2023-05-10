a3354699bc0fa425e623acc478fb3950112506dd
adityarai3 <101617731+adityarai3@users.noreply.github.com>
2023-05-10 22:11:22 +0530

    Update exp11.py

--- exp11.py
+++ exp11.py
@@ -1,11 +1,10 @@
-from sklearn.datasets import load_boston
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
-from sklearn.linear_model import BayesianRidge
-dataset = load_boston()
-X, y = dataset.data, dataset.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.15, random_state = 42)
-model = BayesianRidge()
-model.fit(X_train, y_train)
-prediction = model.predict(X_test)
-print(f"r2 Score Of Test Set : {r2_score(y_test, prediction)}")
+from sklearn.metrics import accuracy_score
+
+# Define the ground truth labels and predicted labels
+y_true = [0, 1, 0, 1, 1, 0, 1, 0]
+y_pred = [0, 1, 0, 1, 0, 1, 1, 0]
+
+# Calculate the accuracy
+accuracy = accuracy_score(y_true, y_pred)
+
+print("Accuracy: {:.2f}".format(accuracy))
